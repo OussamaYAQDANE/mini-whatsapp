2c0304e0e78b99f8bea3e0e9b6f2bbc7c4ad89b9
Adam-0203 <hajjajiadam.23@gmail.com>
2025-04-19 19:31:04 +0100

    cards

---
+++ src/components/UserCard.vue
@@ -0,0 +1,17 @@
+<script setup>
+  /* eslint-disable */
+  import { defineProps } from 'vue';
+//   const user
+</script>
+
+<template>
+    <div>
+
+    </div>
+</template>
+
+
+
+<style scoped>
+
+</style>
--- src/views/SearchPage.vue
+++ src/views/SearchPage.vue
@@ -1,23 +1,36 @@
 <script setup>
-/* eslint-disable */
-import { addGroups } from '@/utilities/createGroups';
+    /* eslint-disable */
+    import GroupCard from '@/components/GroupCard.vue';
+    import UserCard from '@/components/UserCard.vue';
+    import {ref} from 'vue';
+    import { db } from '@/firebase/firebase-config';
+    import { collection, getDocs } from 'firebase/firestore';
+    import { addGroups } from '@/utilities/createGroups';
+    const groupSelected = ref(false);
+    const userSelected = ref(true);
+    const input = ref("");
+    const result_of_Search = await getDocs(collection(db, "users"));
+
+
+
 </script>
 
 <template>
     <main>
         <div class="searchDiv">
-            <div class="search">
-                <input type="text">
+            <div class="search"> 
+                <input type="text" v-model="input">
                 <span>🔎</span>
             </div>
             <div class="type">
-                <div class="groupes"></div>
-                <div class="people"></div>
+                <div class="groupes">Groupes</div>
+                <div class="people">Users</div>
             </div>
         </div>
 
         <div class="display">
-            
+            <GroupCard v-if="groupSelected" v-for="x in result_of_Search" :key="x.id"/>
+            <UserCard v-if="userSelected" v-for="x in result_of_Search" :key="x.id"/>
         </div>
 
     </main>
